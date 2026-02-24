5eaaa84ad76ed0f3ddba02d7c9cca200aaca503f
allenmoench <moenchallen@gmail.com>
2026-02-24 10:41:23 -0700

    Add my name

--- boulder_temperatures.py
+++ boulder_temperatures.py
@@ -10,7 +10,7 @@ Instructions:
 """
 
 # TODO: Add your name here
-STUDENT_NAME = ""  # Example: "Alice Johnson"
+STUDENT_NAME = "Michael Moench"  # Example: "Alice Johnson"
 
 
 def load_temperatures(filepath):
